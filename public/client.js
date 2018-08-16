0f60ab078a5cee9b12b7ab5414db42ab44adec6c
Glitch (webrtc-from-chat) <none>
2018-08-16 23:54:07 +0000

    🕰🚃 Checkpoint ./public/client.js:1055944/271

--- public/client.js
+++ public/client.js
@@ -98,8 +98,8 @@ function connect() {
   var scheme = "ws";
 
   // If this is an HTTPS connection, we have to use a secure WebSocket
-  // connection too, so add another "s" to the scheme.
-
+  // connection too, so add another "s" to the scheme. 
+  
   if (document.location.protocol === "https:") {
     scheme += "s";
   }
