c2fc4be7ac43ec555771c3b32d4ffda4b3a73ef2
Glitch (webrtc-from-chat) <none>
2018-08-17 21:30:34 +0000

    🏑🎎 Checkpoint ./server.js:1055944/1549

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ function sendUserListToAll() {
   var i;
 
   for (i=0; i<connectionArray.length; i++) {
-    connectionArray[i].sendUTF(userListMsgStr);
+    connectionArray[i].send(userListMsgStr);
   }
 }
 
@@ -174,81 +174,80 @@ wss.on("connection", function connection(ws) {
   ws.send(JSON.stringify(msg));
   
   ws.on("message", function(message) {
-    if (message.type === 'utf8') {
-      log("Received Message: " + message.utf8Data);
+    log("Message received:");
+    log(message);
 
-      // Process incoming data.
+    // Convert the JSON back to an object and process it.
+    
+    var sendToClients = true;
+    var msg = JSON.parse(message);
+    var connect = getConnectionForID(msg.id);
 
-      var sendToClients = true;
-      var msg = JSON.parse(message.utf8Data);
-      var connect = getConnectionForID(msg.id);
+    // Take a look at the incoming object and act on it based
+    // on its type. Unknown message types are passed through,
+    // since they may be used to implement client-side features.
+    // Messages with a "target" property are sent only to a user
+    // by that name.
 
-      // Take a look at the incoming object and act on it based
-      // on its type. Unknown message types are passed through,
-      // since they may be used to implement client-side features.
-      // Messages with a "target" property are sent only to a user
-      // by that name.
+    switch(msg.type) {
+      // Public, textual message
+      case "message":
+        msg.name = connect.username;
+        msg.text = msg.text.replace(/(<([^>]+)>)/ig, "");
+        break;
 
-      switch(msg.type) {
-        // Public, textual message
-        case "message":
-          msg.name = connect.username;
-          msg.text = msg.text.replace(/(<([^>]+)>)/ig, "");
-          break;
+      // Username change
+      case "username":
+        var nameChanged = false;
+        var origName = msg.name;
 
-        // Username change
-        case "username":
-          var nameChanged = false;
-          var origName = msg.name;
+        // Ensure the name is unique by appending a number to it
+        // if it's not; keep trying that until it works.
+        while (!isUsernameUnique(msg.name)) {
+          msg.name = origName + appendToMakeUnique;
+          appendToMakeUnique++;
+          nameChanged = true;
+        }
 
-          // Ensure the name is unique by appending a number to it
-          // if it's not; keep trying that until it works.
-          while (!isUsernameUnique(msg.name)) {
-            msg.name = origName + appendToMakeUnique;
-            appendToMakeUnique++;
-            nameChanged = true;
-          }
+        // If the name had to be changed, we send a "rejectusername"
+        // message back to the user so they know their name has been
+        // altered by the server.
+        if (nameChanged) {
+          var changeMsg = {
+            id: msg.id,
+            type: "rejectusername",
+            name: msg.name
+          };
+          connect.se(JSON.stringify(changeMsg));
+        }
 
-          // If the name had to be changed, we send a "rejectusername"
-          // message back to the user so they know their name has been
-          // altered by the server.
-          if (nameChanged) {
-            var changeMsg = {
-              id: msg.id,
-              type: "rejectusername",
-              name: msg.name
-            };
-            connect.sendUTF(JSON.stringify(changeMsg));
-          }
+        // Set this connection's final username and send out the
+        // updated user list to all users. Yeah, we're sending a full
+        // list instead of just updating. It's horribly inefficient
+        // but this is a demo. Don't do this in a real app.
+        connect.username = msg.name;
+        sendUserListToAll();
+        sendToClients = false;  // We already sent the proper responses
+        break;
+    }
 
-          // Set this connection's final username and send out the
-          // updated user list to all users. Yeah, we're sending a full
-          // list instead of just updating. It's horribly inefficient
-          // but this is a demo. Don't do this in a real app.
-          connect.username = msg.name;
-          sendUserListToAll();
-          sendToClients = false;  // We already sent the proper responses
-          break;
-      }
+    // Convert the revised message back to JSON and send it out
+    // to the specified client or all clients, as appropriate. We
+    // pass through any messages not specifically handled
+    // in the select block above. This allows the clients to
+    // exchange signaling and other control objects unimpeded.
 
-      // Convert the revised message back to JSON and send it out
-      // to the specified client or all clients, as appropriate. We
-      // pass through any messages not specifically handled
-      // in the select block above. This allows the clients to
-      // exchange signaling and other control objects unimpeded.
+    if (sendToClients) {
+      var msgString = JSON.stringify(msg);
+      var i;
 
-      if (sendToClients) {
-        var msgString = JSON.stringify(msg);
-        var i;
-
-        // If the message specifies a target username, only send the
-        // message to them. Otherwise, send it to every user.
-        if (msg.target && msg.target !== undefined && msg.target.length !== 0) {
-          sendToOneUser(msg.target, msgString);
-        } else {
-          for (i=0; i<connectionArray.length; i++) {
-            connectionArray[i].sendUTF(msgString);
-          }
+      // If the message specifies a target username, only send the
+      // message to them. Otherwise, send it to every user.
+      if (msg.target && msg.target !== undefined && msg.target.length !== 0) {
+        sendToOneUser(msg.target, msgString);
+      } else {
+        for (i=0; i<connectionArray.length; i++) {
+          connectionArray[i].sendUTF(msgString);
         }
       }
     }
