22be8da9a23eed3ea9f0ebe67883d38f5e1a71f4
Glitch (webrtc-from-chat) <none>
2018-08-17 16:30:12 +0000

    📊⏱ Checkpoint ./server.js:1055944/3226

--- server.js
+++ server.js
@@ -140,7 +140,100 @@ function sendUserListToAll() {
 
 // Create the HTTP server.
 
+var app = express();
+app.use(serveStatic("public"));
+app.use(serveStatic("views"));
 
+// Create the WebSocket server.
+
+var expressWS = require('express-ws')(app);
+
+app.ws("/", function(ws, request) {
+  ws.on("message", function(message) {
+    if (message.type === 'utf8') {
+      log("Received Message: " + message.utf8Data);
+
+      // Process incoming data.
+
+      var sendToClients = true;
+      var msg = JSON.parse(message.utf8Data);
+      var connect = getConnectionForID(msg.id);
+
+      // Take a look at the incoming object and act on it based
+      // on its type. Unknown message types are passed through,
+      // since they may be used to implement client-side features.
+      // Messages with a "target" property are sent only to a user
+      // by that name.
+
+      switch(msg.type) {
+        // Public, textual message
+        case "message":
+          msg.name = connect.username;
+          msg.text = msg.text.replace(/(<([^>]+)>)/ig, "");
+          break;
+
+        // Username change
+        case "username":
+          var nameChanged = false;
+          var origName = msg.name;
+
+          // Ensure the name is unique by appending a number to it
+          // if it's not; keep trying that until it works.
+          while (!isUsernameUnique(msg.name)) {
+            msg.name = origName + appendToMakeUnique;
+            appendToMakeUnique++;
+            nameChanged = true;
+          }
+
+          // If the name had to be changed, we send a "rejectusername"
+          // message back to the user so they know their name has been
+          // altered by the server.
+          if (nameChanged) {
+            var changeMsg = {
+              id: msg.id,
+              type: "rejectusername",
+              name: msg.name
+            };
+            connect.sendUTF(JSON.stringify(changeMsg));
+          }
+
+          // Set this connection's final username and send out the
+          // updated user list to all users. Yeah, we're sending a full
+          // list instead of just updating. It's horribly inefficient
+          // but this is a demo. Don't do this in a real app.
+          connect.username = msg.name;
+          sendUserListToAll();
+          sendToClients = false;  // We already sent the proper responses
+          break;
+      }
+
+      // Convert the revised message back to JSON and send it out
+      // to the specified client or all clients, as appropriate. We
+      // pass through any messages not specifically handled
+      // in the select block above. This allows the clients to
+      // exchange signaling and other control objects unimpeded.
+
+      if (sendToClients) {
+        var msgString = JSON.stringify(msg);
+        var i;
+
+        // If the message specifies a target username, only send the
+        // message to them. Otherwise, send it to every user.
+        if (msg.target && msg.target !== undefined && msg.target.length !== 0) {
+          sendToOneUser(msg.target, msgString);
+        } else {
+          for (i=0; i<connectionArray.length; i++) {
+            connectionArray[i].sendUTF(msgString);
+          }
+        }
+      }
+    }
+  });
+});
+
+// Listen for connections.
+
+app.listen(process.env.PORT);
 
 // Set up a "connect" message handler on our WebSocket server. This is
 // called whenever a user connects to the server's port using the
